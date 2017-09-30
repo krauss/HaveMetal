4549111876e72e6d0155f525884222d4da311159
krauss <jose.ricardo.k@gmail.com>
2017-09-30 22:18:03 +1000

    Created the Login page and modified the existing tabs[2].

---
+++ src/pages/jobs/jobs.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-jobs',
+  templateUrl: 'jobs.html'
+})
+export class JobsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/scanner/scanner.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-scanner',
+  templateUrl: 'scanner.html'
+})
+export class ScannerPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
